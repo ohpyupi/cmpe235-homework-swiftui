db449f79ae65e6b91abbafe68bf0e0d21d78649e
Hoseong Lee <ohpyupi@gmail.com>
2020-07-12 20:32:05 -0700

    Initial Commit

---
+++ cmpe235-homework-swiftui/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  cmpe235-homework-swiftui
+//
+//  Created by Lee, Hoseong on 7/12/20.
+//  Copyright © 2020 Lee, Hoseong. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ cmpe235-homework-swiftuiTests/cmpe235_homework_swiftuiTests.swift
@@ -0,0 +1,34 @@
+//
+//  cmpe235_homework_swiftuiTests.swift
+//  cmpe235-homework-swiftuiTests
+//
+//  Created by Lee, Hoseong on 7/12/20.
+//  Copyright © 2020 Lee, Hoseong. All rights reserved.
+//
+
+import XCTest
+@testable import cmpe235_homework_swiftui
+
+class cmpe235_homework_swiftuiTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
