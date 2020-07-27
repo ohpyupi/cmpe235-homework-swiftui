b5683fb797fc88aa8bb8318c5f7912efb2cd5fad
Hoseong Lee <ohpyupi@gmail.com>
2020-07-26 17:45:12 -0700

    Added a ML model to predict WoW character's race and its training data set
    Added a feature to predict a character's race using a custom-trained CoreML model

--- cmpe235-homework-swiftui/UserCharacterDetail.swift
+++ cmpe235-homework-swiftui/UserCharacterDetail.swift
@@ -7,8 +7,11 @@
 //
 
 import SwiftUI
+import CoreML
+import Vision
 
 struct UserCharacterDetail: View {
+    @State private var predirectedRace = "Unknown";
     let gradient = Gradient(colors: [.blue, .purple])
     var userCharacter: UserCharacter
     var body: some View {
@@ -26,7 +29,7 @@ struct UserCharacterDetail: View {
                 Text(userCharacter.name)
                     .font(.title)
                 HStack {
-                    Text("\(userCharacter.race) \(userCharacter.className)")
+                    Text("\(predirectedRace) \(userCharacter.className)")
                         .font(.subheadline)
                     Spacer()
                     Text("\(userCharacter.serverName) - \(userCharacter.faction)")
@@ -37,6 +40,28 @@ struct UserCharacterDetail: View {
             Spacer()
         }
         .navigationBarTitle(Text(userCharacter.name), displayMode: .inline)
+        .onAppear() {
+            self.predictCharacterRace(imageName: self.userCharacter.imageName)
+        }
+    }
+    
+    func predictCharacterRace(imageName: String) {
+        DispatchQueue.global(qos: .userInitiated).async {
+            do {
+                let cgImage = ImageStore.loadImage(name: imageName)
+                func handlePredirection(request: VNRequest, error: Error?) {
+                    let results = request.results as? [VNClassificationObservation]
+                    let topPredictedRace = results?[0]
+                    self.predirectedRace = topPredictedRace!.identifier
+                }
+                let model = try VNCoreMLModel(for: WowCharacterRace().model)
+                let request = VNCoreMLRequest(model: model, completionHandler: handlePredirection)
+                let handler = VNImageRequestHandler(cgImage: cgImage)
+                try handler.perform([request])
+            } catch {
+                fatalError("Something went wrong")
+            }
+        }
     }
 }
 
